3c2cfe59a70e9615ce3dc4e2757103a78bd06c63
Dandarawy <abdulla.aldandarawy@gmail.com>
2016-09-19 15:03:54 +0200

    Adding main functionalities

---
+++ Assets/Scripts/PositionController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PositionController : MonoBehaviour {
+    public Slider XPos;
+    public Slider YPos;
+    public Slider ZPos;
+    //public Transform ControlledObject;
+    public void UpdateObjectPosition(Transform ControlledObject)
+    {
+        Vector3 newPosition = new Vector3(XPos.value, YPos.value, ZPos.value);
+        ControlledObject.localPosition = newPosition;
+    }
+}
